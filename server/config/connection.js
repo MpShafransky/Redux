8a5a726e6ad38ac851c6a38d5d7dd898fd8f0f9b
Matthew Shafransky <matt@Matthews-MacBook-Pro.local>
2021-09-01 14:23:59 -0400

    fixed code

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Redux-Store', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -8,3 +8,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', 
 });
 
 module.exports = mongoose.connection;
+
+
